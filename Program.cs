32ec79cb2f3c64b34dcce0a51b9d7978109ca6a6
KevinAguayo <89602195+KevinAguayo@users.noreply.github.com>
2021-08-30 02:07:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Suma1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sumar
+            int a = 132;
+            int b = 434;
+            int c = a + b;
+            Console.WriteLine(a + "/" + b + "=" + c);
+
+
+            Console.ReadKey();
+                
+
+        }
+    }
+}
